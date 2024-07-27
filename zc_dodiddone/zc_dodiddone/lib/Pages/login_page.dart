f833eda5a89ffcbe107ee842bfd10275bba4c7c6
Dmitry Korobov <dmitriy.korobov@effem.com>
2024-07-27 16:52:08 +0300

    update

---
+++ zc_dodiddone/zc_dodiddone/lib/Pages/login_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:zc_dodiddone/Theme/theme.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  bool _isLogin = true; // Flag to indicate login or registration
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose(); // Освобождаем контроллер
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: _isLogin
+                ? [
+                    DoDidDoneTheme.lightTheme.colorScheme.secondary,
+                    DoDidDoneTheme.lightTheme.colorScheme.primary,
+                  ]
+                : [
+                    DoDidDoneTheme.lightTheme.colorScheme.primary,
+                    DoDidDoneTheme.lightTheme.colorScheme.secondary,
+                  ],
+            stops: const [0.1, 0.9],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  _isLogin ? 'Вход' : 'Регистрация',
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Адрес электронной почты', // Изменили подсказку
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите адрес электронной почты';
+                    }
+                    if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$").hasMatch(value)) {
+                      return 'Некорректный адрес электронной почты';
+                    }
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Пароль',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите пароль';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _confirmPasswordController, // Используем новый контроллер
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Повторить пароль', // Новая подсказка
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, повторите пароль';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Пароли не совпадают';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Обработка входа или регистрации
+                      // ...
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: 
+                    _isLogin ? DoDidDoneTheme.lightTheme.colorScheme.secondary :  DoDidDoneTheme.lightTheme.colorScheme.primary,
+                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    textStyle: const TextStyle(fontSize: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                  ),
+                  child: Text(_isLogin ? 'Войти' : 'Зарегистрироваться'),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isLogin = !_isLogin; // Toggle login/registration
+                    });
+                  },
+                  child: Text(
+                    _isLogin?'У меня еще нет аккаунта...': 'Уже есть аккаунт...',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ zc_dodiddone/zc_dodiddone/lib/Theme/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DoDidDoneTheme {
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF9f7bf6), // Primary color
+      brightness: Brightness.light,
+      primary: const Color(0xFF9F7BF6),  // Primary color
+      secondary: const Color(0xFF4ceb8b), // Secondary color
+    ),
+    useMaterial3: true,
+    // Customize the ElevatedButton style
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white, // Set button text color to white
+      ),
+    ),
+  );
+}
