2d16395bf1bf7edd30a10726cd635c24e8c040f3
Dmitry Korobov <dmitriy.korobov@effem.com>
2024-07-28 23:36:00 +0300

    swipe

--- zc_dodiddone/zc_dodiddone/lib/Screens/all_tasks.dart
+++ zc_dodiddone/zc_dodiddone/lib/Screens/all_tasks.dart
@@ -13,10 +13,34 @@ class _TaskPageState extends State<TaskPage> {
   final CollectionReference _tasksCollection =
       FirebaseFirestore.instance.collection('tasks');
 
+  // Функция для удаления задачи
+  Future<void> _deleteTask(String taskId) async {
+    try {
+      await _tasksCollection.doc(taskId).delete();
+      // Можно добавить сообщение об успешном удалении
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Задача удалена')),
+      );
+    } catch (e) {
+      // Обработка ошибки удаления
+      print('Ошибка удаления задачи: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Ошибка удаления: $e')),
+      );
+    }
+  }
+
+  // Функция для редактирования задачи
+  void _editTask(String taskId) {
+    // Переход на страницу редактирования задачи
+    Navigator.pushNamed(context, '/edit_task', arguments: taskId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: _tasksCollection.snapshots(),
+      stream: _tasksCollection.where('completed', isEqualTo: false).
+      snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return const Center(child: Text('Ошибка загрузки задач'));
@@ -26,86 +50,75 @@ class _TaskPageState extends State<TaskPage> {
           return const Center(child: CircularProgressIndicator());
         }
 
-
         // Проверяем, есть ли данные в snapshot
         final tasks = snapshot.data!.docs;
 
         if (tasks.isEmpty) {
-          return const Center(child: Text('Нет задач, время отдыхать\n или создай задачу!'));
+          return const Center(
+              child: Text('Нет задач, время отдыхать\n или создай задачу!'));
         }
 
-
         return ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index) {
-                    final taskData = tasks[index].data() as Map<String, dynamic>;
-                    final taskTitle = taskData['title'];
-                    final taskDescription = taskData['description'];
-                    final taskDeadline =
-                        (taskData['deadline'] as Timestamp).toDate();
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            final taskData = tasks[index].data() as Map<String, dynamic>;
+            final taskId = tasks[index].id;
+            final taskTitle = taskData['title'];
+            final taskDescription = taskData['description'];
+            final taskDeadline =
+                (taskData['deadline'] as Timestamp).toDate();
 
-                    return TaskItem(
-                      title: taskTitle,
-                      description: taskDescription,
-                      deadline: taskDeadline,
-                    );
-                  },
+            return Dismissible(
+              key: Key(taskId), // Уникальный ключ для Dismissible
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+              secondaryBackground: Container(
+                color: Colors.blue,
+                alignment: Alignment.centerLeft,
+                child: const Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                ),
+              ),
+              onDismissed: (direction) {
+                if (direction == DismissDirection.endToStart) {
+                  _deleteTask(taskId); // Вызываем _deleteTask при свайпе влево
+                } else if (direction == DismissDirection.startToEnd) {
+                  _editTask(taskId); // Вызываем _editTask при свайпе вправо
+                }
+              },
+              child: TaskItem(
+                title: taskTitle,
+                description: taskDescription,
+                deadline: taskDeadline,
+                
+                onEdit: () {
+                  _editTask(taskId); // Вызываем _editTask при нажатии на кнопку редактирования
+                },
+                onDelete: () {
+                  _deleteTask(taskId); // Вызываем _deleteTask при нажатии на кнопку удаления
+                }, 
+                to_left: (){
+                  _tasksCollection
+                  .doc(tasks[index].id)
+                  .update({'completed': true});
+                }, 
+                to_right: (){
+                  _tasksCollection
+                  .doc(tasks[index].id)
+                  .update({'is_for_today': true});
+                },
+              ),
+            );
+          },
         );
-        
       },
     );
   }
 }
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:zc_dodiddone/Widgets/task_item.dart';
-
-
-// class TaskPage extends StatelessWidget {
-//   const TaskPage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Список задач
-//     final List<String> tasks = [
-//       'Купить продукты',
-//       'Записаться на тренировку',
-//       'Отправить отчет',
-//     ];
-
-//     return Padding(
-//       padding: const EdgeInsets.all(20.0),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           // Заголовок
-//           const Text(
-//             'Задачи',
-//             style: TextStyle(
-//               fontSize: 24,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-
-//           // Список задач
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: tasks.length, // Используем длину массива tasks
-//               itemBuilder: (context, index) {
-//                 return TaskItem(
-//                   title: tasks[index],
-//                   description: 'Описание задачи',
-//                   deadline: DateTime.now(),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
--- zc_dodiddone/zc_dodiddone/lib/Widgets/task_item.dart
+++ zc_dodiddone/zc_dodiddone/lib/Widgets/task_item.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -5,12 +6,20 @@ class TaskItem extends StatelessWidget {
   final String title;
   final String description;
   final DateTime deadline;
+  final VoidCallback onEdit; // Добавляем onEdit
+  final VoidCallback onDelete; // Добавляем onDelete
+  final Function? to_left;
+  final Function? to_right;
 
   const TaskItem({
     Key? key,
     required this.title,
     required this.description,
     required this.deadline,
+    required this.onEdit, // Передаем onEdit
+    required this.onDelete, // Передаем onDelete
+    required this.to_left,
+    required this.to_right,
   }) : super(key: key);
 
   @override
@@ -21,72 +30,98 @@ class TaskItem extends StatelessWidget {
     // Определяем градиент в зависимости от срочности
     Gradient gradient = getGradient(deadline);
 
-    return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            // Добавляем Container для градиента
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            decoration: BoxDecoration(
-              gradient: gradient,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(8.0),
-                topRight: Radius.circular(8.0),
+    return Dismissible(
+      key: Key(title), // Уникальный ключ для Dismissible
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.blue,
+        alignment: Alignment.centerLeft,
+        child: const Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+      ),
+      onDismissed: (direction) {
+        if (direction == DismissDirection.endToStart) {
+          to_left?.call(); // Вызываем to_left при свайпе влево
+        } else if (direction == DismissDirection.startToEnd) {
+          to_right?.call(); // Вызываем to_right при свайпе вправо
+        }
+      },
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              // Добавляем Container для градиента
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              decoration: BoxDecoration(
+                gradient: gradient,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
               ),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 0, 0, 0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
                     ),
                   ),
-                ),
-                // Добавляем кнопки "Выполнено" и "Редактировать" в заголовок
-                IconButton(
-                  onPressed: () {
-                    // Обработка нажатия на кнопку "Выполнено"
-                    // Например, можно удалить задачу из списка
-                    print('Задача выполнена!');
-                  },
-                  icon: const Icon(Icons.check_circle),
-                ),
-                IconButton(
-                  onPressed: () {
-                    // Обработка нажатия на кнопку "Редактировать"
-                    // Например, можно открыть диалог редактирования задачи
-                    print('Задача редактируется!');
-                  },
-                  icon: const Icon(Icons.edit),
-                ),
-              ],
+                  // Добавляем кнопки "Выполнено" и "Редактировать" в заголовок
+                  IconButton(
+                    onPressed: () {
+                      // Обработка нажатия на кнопку "Выполнено"
+                      // Например, можно удалить задачу из списка
+                      print('Задача выполнена!');
+                    },
+                    icon: const Icon(Icons.check_circle),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Обработка нажатия на кнопку "Редактировать"
+                      // Например, можно открыть диалог редактирования задачи
+                      print('Задача редактируется!');
+                    },
+                    icon: const Icon(Icons.edit),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  description,
-                  style: const TextStyle(fontSize: 14),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Дедлайн: $formattedDeadline',
-                  style: const TextStyle(fontSize: 14),
-                ),
-              ],
-            ),
-          )
-        ],
+            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    description,
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Дедлайн: $formattedDeadline',
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -116,3 +151,123 @@ class TaskItem extends StatelessWidget {
     }
   }
 }
+
+
+// import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+
+// class TaskItem extends StatelessWidget {
+//   final String title;
+//   final String description;
+//   final DateTime deadline;
+
+//   const TaskItem({
+//     Key? key,
+//     required this.title,
+//     required this.description,
+//     required this.deadline,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Форматируем дату и время с помощью intl
+//     String formattedDeadline = DateFormat('dd.MM.yy HH:mm').format(deadline);
+
+//     // Определяем градиент в зависимости от срочности
+//     Gradient gradient = getGradient(deadline);
+
+//     return Card(
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Container(
+//             // Добавляем Container для градиента
+//             padding:
+//                 const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//             decoration: BoxDecoration(
+//               gradient: gradient,
+//               borderRadius: BorderRadius.only(
+//                 topLeft: Radius.circular(8.0),
+//                 topRight: Radius.circular(8.0),
+//               ),
+//             ),
+//             child: Row(
+//               children: [
+//                 Expanded(
+//                   child: Text(
+//                     title,
+//                     style: const TextStyle(
+//                       fontSize: 18,
+//                       fontWeight: FontWeight.bold,
+//                       color: Color.fromARGB(255, 0, 0, 0),
+//                     ),
+//                   ),
+//                 ),
+//                 // Добавляем кнопки "Выполнено" и "Редактировать" в заголовок
+//                 IconButton(
+//                   onPressed: () {
+//                     // Обработка нажатия на кнопку "Выполнено"
+//                     // Например, можно удалить задачу из списка
+//                     print('Задача выполнена!');
+//                   },
+//                   icon: const Icon(Icons.check_circle),
+//                 ),
+//                 IconButton(
+//                   onPressed: () {
+//                     // Обработка нажатия на кнопку "Редактировать"
+//                     // Например, можно открыть диалог редактирования задачи
+//                     print('Задача редактируется!');
+//                   },
+//                   icon: const Icon(Icons.edit),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           const SizedBox(height: 8),
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   description,
+//                   style: const TextStyle(fontSize: 14),
+//                 ),
+//                 const SizedBox(height: 8),
+//                 Text(
+//                   'Дедлайн: $formattedDeadline',
+//                   style: const TextStyle(fontSize: 14),
+//                 ),
+//               ],
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+
+//   // Функция для получения градиента в зависимости от срочности
+//   Gradient getGradient(DateTime deadline) {
+//     Duration timeUntilDeadline = deadline.difference(DateTime.now());
+
+//     if (timeUntilDeadline.inDays <= 1) {
+//       return const LinearGradient(
+//         colors: [Colors.red, Colors.white],
+//         begin: Alignment.topCenter,
+//         end: Alignment.bottomCenter,
+//       );
+//     } else if (timeUntilDeadline.inDays <= 2) {
+//       return const LinearGradient(
+//         colors: [Colors.yellow, Colors.white],
+//         begin: Alignment.topCenter,
+//         end: Alignment.bottomCenter,
+//       );
+//     } else {
+//       return const LinearGradient(
+//         colors: [Colors.green, Colors.white],
+//         begin: Alignment.topCenter,
+//         end: Alignment.bottomCenter,
+//       );
+//     }
+//   }
+// }
